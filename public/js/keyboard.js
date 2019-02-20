5d45a2f5130b832db3448471311fc4a9c02cf322
Jaclyn Chin <jac289@ucsd.edu>
2019-02-20 14:14:59 -0800

    eep

--- public/js/keyboard.js
+++ public/js/keyboard.js
@@ -1,4 +1,19 @@
 
+$('#fileUpload').on('change',function(){
+  $('.avatar').removeClass('open');
+});
+$('.avatar').on('click',function(){
+  $(this).addClass('open');
+});
+// added code to close the modal if you click outside
+$('html').click(function() {
+ $('.avatar').removeClass('open');
+});
+
+$('.avatar').click(function(event){
+    event.stopPropagation();
+});
+
 
 var random = document.querySelector('#random');
 
