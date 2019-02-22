6d26f9e79c99b9bf4d09d3f9110d9177734892ba
Jaclyn Chin <jac289@ucsd.edu>
2019-02-22 01:13:32 -0800

    hi

--- public/js/keyboard.js
+++ public/js/keyboard.js
@@ -1,23 +1,10 @@
-
-$('#fileUpload').on('change',function(){
-  $('.avatar').removeClass('open');
-});
-$('.avatar').on('click',function(){
-  $(this).addClass('open');
-});
-// added code to close the modal if you click outside
-$('html').click(function() {
- $('.avatar').removeClass('open');
-});
-
-$('.avatar').click(function(event){
-    event.stopPropagation();
-});
+var help = [
+'Wow, that would really help :)'];
 
 
 var random = document.querySelector('#random');
 
-random.innerHTML = memes[Math.floor(Math.random() * memes.length)];
+random.innerHTML = help[Math.floor(Math.random() * help.length)];
 
 /* Time */
 
@@ -26,12 +13,12 @@ var messageTime = document.querySelectorAll('.message .time');
 
 deviceTime.innerHTML = moment().format('h:mm');
 
-setInterval(function() {
-	deviceTime.innerHTML = moment().format('h:mm');
+setInterval(function () {
+  deviceTime.innerHTML = moment().format('h:mm');
 }, 1000);
 
 for (var i = 0; i < messageTime.length; i++) {
-	messageTime[i].innerHTML = moment().format('h:mm A');
+  messageTime[i].innerHTML = moment().format('h:mm A');
 }
 
 /* Message */
@@ -42,40 +29,40 @@ var conversation = document.querySelector('.conversation-container');
 form.addEventListener('submit', newMessage);
 
 function newMessage(e) {
-	var input = e.target.input;
+  var input = e.target.input;
 
-	if (input.value) {
-		var message = buildMessage(input.value);
-		conversation.appendChild(message);
-		animateMessage(message);
-	}
+  if (input.value) {
+    var message = buildMessage(input.value);
+    conversation.appendChild(message);
+    animateMessage(message);
+  }
 
-	input.value = '';
-	conversation.scrollTop = conversation.scrollHeight;
+  input.value = '';
+  conversation.scrollTop = conversation.scrollHeight;
 
-	e.preventDefault();
+  e.preventDefault();
 }
 
 function buildMessage(text) {
-	var element = document.createElement('div');
+  var element = document.createElement('div');
 
-	element.classList.add('message', 'sent');
+  element.classList.add('message', 'sent');
 
-	element.innerHTML = text +
-		'<span class="metadata">' +
-			'<span class="time">' + moment().format('h:mm A') + '</span>' +
-			'<span class="tick tick-animation">' +
-				'<svg xmlns="http://www.w3.org/2000/svg" width="16" height="15" id="msg-dblcheck" x="2047" y="2061"><path d="M15.01 3.316l-.478-.372a.365.365 0 0 0-.51.063L8.666 9.88a.32.32 0 0 1-.484.032l-.358-.325a.32.32 0 0 0-.484.032l-.378.48a.418.418 0 0 0 .036.54l1.32 1.267a.32.32 0 0 0 .484-.034l6.272-8.048a.366.366 0 0 0-.064-.512zm-4.1 0l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.88a.32.32 0 0 1-.484.032L1.892 7.77a.366.366 0 0 0-.516.005l-.423.433a.364.364 0 0 0 .006.514l3.255 3.185a.32.32 0 0 0 .484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z" fill="#92a58c"/></svg>' +
-				'<svg xmlns="http://www.w3.org/2000/svg" width="16" height="15" id="msg-dblcheck-ack" x="2063" y="2076"><path d="M15.01 3.316l-.478-.372a.365.365 0 0 0-.51.063L8.666 9.88a.32.32 0 0 1-.484.032l-.358-.325a.32.32 0 0 0-.484.032l-.378.48a.418.418 0 0 0 .036.54l1.32 1.267a.32.32 0 0 0 .484-.034l6.272-8.048a.366.366 0 0 0-.064-.512zm-4.1 0l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.88a.32.32 0 0 1-.484.032L1.892 7.77a.366.366 0 0 0-.516.005l-.423.433a.364.364 0 0 0 .006.514l3.255 3.185a.32.32 0 0 0 .484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z" fill="#4fc3f7"/></svg>' +
-			'</span>' +
-		'</span>';
+  element.innerHTML = text +
+  '<span class="metadata">' +
+  '<span class="time">' + moment().format('h:mm A') + '</span>' +
+  '<span class="tick tick-animation">' +
+  '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="15" id="msg-dblcheck" x="2047" y="2061"><path d="M15.01 3.316l-.478-.372a.365.365 0 0 0-.51.063L8.666 9.88a.32.32 0 0 1-.484.032l-.358-.325a.32.32 0 0 0-.484.032l-.378.48a.418.418 0 0 0 .036.54l1.32 1.267a.32.32 0 0 0 .484-.034l6.272-8.048a.366.366 0 0 0-.064-.512zm-4.1 0l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.88a.32.32 0 0 1-.484.032L1.892 7.77a.366.366 0 0 0-.516.005l-.423.433a.364.364 0 0 0 .006.514l3.255 3.185a.32.32 0 0 0 .484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z" fill="#92a58c"/></svg>' +
+  '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="15" id="msg-dblcheck-ack" x="2063" y="2076"><path d="M15.01 3.316l-.478-.372a.365.365 0 0 0-.51.063L8.666 9.88a.32.32 0 0 1-.484.032l-.358-.325a.32.32 0 0 0-.484.032l-.378.48a.418.418 0 0 0 .036.54l1.32 1.267a.32.32 0 0 0 .484-.034l6.272-8.048a.366.366 0 0 0-.064-.512zm-4.1 0l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.88a.32.32 0 0 1-.484.032L1.892 7.77a.366.366 0 0 0-.516.005l-.423.433a.364.364 0 0 0 .006.514l3.255 3.185a.32.32 0 0 0 .484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z" fill="#4fc3f7"/></svg>' +
+  '</span>' +
+  '</span>';
 
-	return element;
+  return element;
 }
 
 function animateMessage(message) {
-	setTimeout(function() {
-		var tick = message.querySelector('.tick');
-		tick.classList.remove('tick-animation');
-	}, 500);
+  setTimeout(function () {
+    var tick = message.querySelector('.tick');
+    tick.classList.remove('tick-animation');
+  }, 500);
 }
