2117b76d224f6441b53c8370e8a61a81a45fe2a7
Jaclyn Chin <jac289@ucsd.edu>
2019-02-27 11:04:54 -0800

    A/B added

--- routes/home.js
+++ routes/home.js
@@ -3,6 +3,13 @@ var data = require('../data.json');
 
 exports.viewHome = function(req, res) { 
 	console.log(data);
+	data["viewHomeAlt"] = false;
   res.render('home', data);
 
-}
+};
+
+exports.viewHomeAlt = function(request, response){
+	data["viewHomeAlt"] = true;
+  	response.render('home', data);
+  	
+};
