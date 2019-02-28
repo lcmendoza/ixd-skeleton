5a9d640832b0193d322711422886fca374d31cb7
Jaclyn Chin <jac289@ucsd.edu>
2019-02-28 11:31:59 -0800

    hi

--- public/js/reminders.js
+++ public/js/reminders.js
@@ -43,6 +43,16 @@ $(document).ready(function() {
     });
 });
 
+function initializePage() {
+    // your code here
+    $('.likeBtn').click(likeClicked);
+}
 
+function likeClicked(e){
+    e.preventDefault();
+    console.log("clickin");
+    ga('create', 'UA-134865038-1', 'auto');
+    ga("send", "event", "like", "click");
+}
 
 
