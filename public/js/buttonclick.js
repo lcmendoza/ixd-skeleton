09c75ae3a8bf5626885b6f0c4d1d2167ad748a87
Jaclyn Chin <jac289@ucsd.edu>
2019-03-04 14:42:01 -0800

    hi

--- public/js/buttonclick.js
+++ public/js/buttonclick.js
@@ -16,6 +16,6 @@ function initializePage() {
 function likeClicked(e){
 	e.preventDefault();
 	console.log("clickin");
-	ga('create', 'UA-134865038-1', 'auto');
+	ga('create', 'UA-135175651-1', 'auto');
 	ga("send", "event", "like", "click");
 }
